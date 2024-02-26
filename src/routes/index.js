90b33344eebe422603e458c624dfd864aa2a5bfc
hvupham <21522804@gm.uit.edu.vn>
2024-02-26 08:29:25 +0700

    setting router

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+
+//
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
